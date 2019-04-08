a6f40769da4d3e81a453fd12409135f073002308
Kulis Pavel <poul250@yandex.ru>
2019-04-08 22:47:43 +0300

    delete useless import

--- test/main/java/com/pawka/emplinfo/EmplInfoTest.java
+++ test/main/java/com/pawka/emplinfo/EmplInfoTest.java
@@ -2,7 +2,6 @@ package main.java.com.pawka.emplinfo;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
 import java.util.Collection;
 import java.util.HashSet;
